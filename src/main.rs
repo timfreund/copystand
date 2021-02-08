39334b92c01a4423b6b21d96876aa7312b8926e9
Tim Freund <tim@freunds.net>
2021-02-07 21:27:13 -0500

    +rexiv2, print all tag data

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate gphoto;
 
 use clap::Clap;
 use std::path::Path;
+use rexiv2::Metadata;
 
 #[derive(Clap)]
 #[clap(version = "0.1.0", author = "Tim Freund <tim@freunds.net>")]
@@ -26,7 +27,7 @@ fn main() {
     // println!("base_filename: {}", opts.base_filename)
 
 
-    let mut context = gphoto::Context::new().unwrap();
+    // let mut context = gphoto::Context::new().unwrap();
 
     // let mut camera = gphoto::Camera::autodetect(&mut context).unwrap();
     // let capture = camera.capture_image(&mut context).unwrap();
@@ -34,5 +35,25 @@ fn main() {
     // let mut file = gphoto::FileMedia::create(Path::new("demofile.raw")).unwrap();
 
     // camera.download(&mut context, &capture, &mut file).unwrap();
+
+    if let Ok(meta) = Metadata::new_from_path("demofile.jpg") {
+        if let Some(location) = meta.get_gps_info() {
+            println!("Location: {:?}", location);
+        }
+
+        for tag_name in meta.get_exif_tags().unwrap(){
+            let tag_value = meta.get_tag_string(&tag_name).unwrap();
+            println!("{} == {}", tag_name, tag_value);
+        }
+
+        for tag_name in meta.get_iptc_tags().unwrap(){
+            let tag_value = meta.get_tag_string(&tag_name).unwrap();
+            println!("{} == {}", tag_name, tag_value);
+        }
+
+        meta.set_tag_numeric("Exif.Image.PageNumber", 17);
+        meta.set_tag_string("Exif.Image.DocumentName", "The Demonstration Document");
+        meta.save_to_file("demofile.jpg").expect("Couldn't save metadata");
+    }
 }
 
