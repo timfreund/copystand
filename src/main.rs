2afaef09bae81d9d8f00ca08abb46bfb7d1eb155
Tim Freund <tim@freunds.net>
2021-02-08 21:49:47 -0500

    One giant monolithic function that mostly works

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate gphoto;
 
 use clap::Clap;
 use std::fs;
+use std::io;
 use std::path::Path;
 use std::process::Command;
 use rexiv2::Metadata;
@@ -11,8 +12,6 @@ use rexiv2::Metadata;
 struct Opts {
     #[clap(long, default_value="./output")]
     outputdir: String,
-    #[clap(long, default_value="scan-0000.jpg")]
-    base_filename: String,
     #[clap(long, default_value="0")]
     start_page: i32,
     #[clap(long, default_value="1")]
@@ -26,38 +25,50 @@ fn main() {
     // page_increment
 
     let opts: Opts = Opts::parse();
-    // println!("base_filename: {}", opts.base_filename)
-
+    let mut current_page = opts.start_page;
 
     let mut context = gphoto::Context::new().unwrap();
-
     let mut camera = gphoto::Camera::autodetect(&mut context).unwrap();
-    let capture = camera.capture_image(&mut context).unwrap();
-    // let mut file = gphoto::FileMedia::create(Path::new(&*capture.basename())).unwrap();
-    let mut file = gphoto::FileMedia::create(Path::new("capture.cr2")).unwrap();
 
-    camera.download(&mut context, &capture, &mut file).unwrap();
-    Command::new("dcraw").arg("-T").arg("capture.cr2").status();
-    Command::new("convert").arg("capture.tiff").arg("-compress").arg("Zip").arg("final.tiff").status();
+    let mut stdin = io::stdin();
+    loop {
+        println!("Press enter to capture page {}", current_page);
+        let mut input = String::new();
+        stdin.read_line(&mut input);
+        let page_filename = format!("scan-{:04}.tiff", current_page);
 
-    if let Ok(meta) = Metadata::new_from_path("capture.cr2") {
-        if let Some(location) = meta.get_gps_info() {
-            println!("Location: {:?}", location);
+        let capture = camera.capture_image(&mut context).unwrap();
+        // let mut file = gphoto::FileMedia::create(Path::new(&*capture.basename())).unwrap();
+        let mut file = gphoto::FileMedia::create(Path::new("capture.cr2")).unwrap();
+
+        camera.download(&mut context, &capture, &mut file).unwrap();
+        Command::new("dcraw").arg("-T").arg("capture.cr2").status();
+        Command::new("convert").arg("capture.tiff").arg("-compress").arg("Zip").arg(&page_filename).status();
+
+        if let Ok(meta) = Metadata::new_from_path("capture.cr2") {
+            if let Some(location) = meta.get_gps_info() {
+                println!("Location: {:?}", location);
+            }
+
+            for tag_name in meta.get_exif_tags().unwrap(){
+                let tag_value = meta.get_tag_string(&tag_name).unwrap();
+                println!("{} == {}", tag_name, tag_value);
+            }
+
+            for tag_name in meta.get_iptc_tags().unwrap(){
+                let tag_value = meta.get_tag_string(&tag_name).unwrap();
+                println!("{} == {}", tag_name, tag_value);
+            }
+
+            meta.set_tag_numeric("Exif.Image.PageNumber", 17);
+            meta.set_tag_string("Exif.Image.DocumentName", "The Demonstration Document");
+            meta.save_to_file(&page_filename).expect("Couldn't save metadata");
         }
+        fs::remove_file("capture.cr2");
 
-        for tag_name in meta.get_exif_tags().unwrap(){
-            let tag_value = meta.get_tag_string(&tag_name).unwrap();
-            println!("{} == {}", tag_name, tag_value);
-        }
 
-        for tag_name in meta.get_iptc_tags().unwrap(){
-            let tag_value = meta.get_tag_string(&tag_name).unwrap();
-            println!("{} == {}", tag_name, tag_value);
-        }
-
-        meta.set_tag_numeric("Exif.Image.PageNumber", 17);
-        meta.set_tag_string("Exif.Image.DocumentName", "The Demonstration Document");
-        meta.save_to_file("final.tiff").expect("Couldn't save metadata");
+        println!("captured to {}", page_filename);
+        current_page += opts.page_increment;
     }
-    fs::remove_file("capture.cr2");
+
 }
